18c10362b624e0fe1fd5f88abb75a274111b39bd
goodyshweta <goodyshweta@gmail.com>
2022-04-12 01:04:03 +0530

    refactoring

--- src/main/java/com/shweta/microservices/CitizenServiceApplication.java
+++ src/main/java/com/shweta/microservices/CitizenServiceApplication.java
@@ -1,4 +1,4 @@
-package com.shweta.microservices.CitizenService;
+package com.shweta.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/shweta/microservices/CitizenServiceApplicationTests.java
+++ src/test/java/com/shweta/microservices/CitizenServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.shweta.microservices.CitizenService;
+package com.shweta.microservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
