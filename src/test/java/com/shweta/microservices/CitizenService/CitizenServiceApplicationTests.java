799ac4e7de8ac84c95c1b7322688a53765fc6b99
goodyshweta <goodyshweta@gmail.com>
2022-04-12 00:58:26 +0530

    Initial setup

---
+++ src/test/java/com/shweta/microservices/CitizenService/CitizenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shweta.microservices.CitizenService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
